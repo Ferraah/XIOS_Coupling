a5e49f769ec66fab24dc6759f9b00285d802fd5b
Ferraah <ferrariodaniele01@gmail.com>
2025-04-08 17:07:34 +0200

    Absolute values ex2

--- 2_singlefield_restart/2_singlefield_restart.f90
+++ 2_singlefield_restart/2_singlefield_restart.f90
@@ -128,17 +128,38 @@ contains
         call xios_set_timestep(config%timestep)
         call xios_set_domain_attr("domain", ni_glo=config%ni_glo, nj_glo=config%nj_glo, type=config%field_type)
         call xios_close_context_definition()
-        print *, "XIOS context definition closed"
 
     end subroutine configure_xios_from_model
  
+    ! This subroutine calculates the elapsed timesteps between the time origin and the current date
+    ! Used by the sender toymodel
+    subroutine calulate_absolute_curr_timestep(curr_date, timestep, elapsed_timesteps) 
+        implicit none
+        type(xios_date), intent(in) :: curr_date
+        type(xios_date) :: tmp_date, start_date 
+        type(xios_duration), intent(in) :: timestep
+        integer, intent(out):: elapsed_timesteps
+        integer :: start_date_sec, curr_date_sec, tmp_date_sec, elapsed_duration_sec
+        type(xios_duration) :: elapsed_duration
+        
+        CALL xios_get_time_origin(start_date)
+        ! Some bad code because there is no xios_duration_convert_to_seconds
+        start_date_sec = xios_date_convert_to_seconds(start_date)
+        curr_date_sec = xios_date_convert_to_seconds(curr_date)
+        elapsed_duration_sec = curr_date_sec - start_date_sec
+        tmp_date = start_date + timestep
+        tmp_date_sec = xios_date_convert_to_seconds(tmp_date)
+        tmp_date_sec = tmp_date_sec - start_date_sec
+        
+        elapsed_timesteps = elapsed_duration_sec / tmp_date_sec + 1 !t=0 is ts=1
+
+    end subroutine calulate_absolute_curr_timestep
 
     subroutine run_coupling(conf)
     implicit none 
         type(toymodel_config) :: conf 
         double precision, pointer:: field_send(:,:), field_recv(:,:)
-        integer :: curr_timestep
-
+        integer :: curr_timestep, absolute_timestep
         allocate(field_send(conf%ni_glo, conf%nj_glo))
         allocate(field_recv(conf%ni_glo, conf%nj_glo))
 
@@ -154,8 +175,9 @@ contains
             call xios_update_calendar(curr_timestep)
 
             if (model_id=="ocn") then
-
-                field_send = curr_timestep
+                call calulate_absolute_curr_timestep(conf%curr_date, conf%timestep, absolute_timestep)     
+                
+                field_send = absolute_timestep
                 print *, "OCN: sending field @ts=", curr_timestep, " with value ", field_send(1,1)
                 call xios_send_field("field2D_send", field_send)
 
@@ -179,4 +201,6 @@ contains
         deallocate(field_recv)
         
     end subroutine  run_coupling 
+
+
 end program
